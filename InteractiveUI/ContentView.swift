619a9bbb2b12b3f3823f9131acabffa07b7c4637
Ruthy <fosdickruth@gmail.com>
2025-06-04 14:46:17 -0500

    created UI

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    
+        VStack {
+            Text("what is your name")
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .font( .title)
+                .border(Color.gray,width: 1)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            
+            }
+           // end button
+            .font (.title)
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
     }
     
 }
